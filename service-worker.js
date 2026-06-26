d38b37342da5f53e8be6fece7f98e5b1d504bd9e
kingsan2012 <kingsan2012@gmail.com>
2026-06-26 13:31:27 +0330

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,27 @@
-const CACHE='smartlist-v1';
-const ASSETS=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = "smartlist-v1";
+
+const urls = [
+    "/",
+    "/index.html",
+    "/manifest.json"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => cache.addAll(urls))
+    );
+});
+
+self.addEventListener("fetch", e => {
+
+    e.respondWith(
+
+        caches.match(e.request).then(res => {
+
+            return res || fetch(e.request);
+
+        })
+
+    );
+
+});
